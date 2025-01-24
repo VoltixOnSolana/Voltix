330a24bdd24e5d0c9c3c3d158c395db31a0488af
ZFran6co <eprixvapegg@gmail.com>
2025-01-24 11:24:38 +0100

    ajout des pages pour stripe à terminer

---
+++ app/cancel/page.tsx
@@ -0,0 +1,9 @@
+export default function CancelPage() {
+    return (
+      <div>
+        <h1>Paiement annulé</h1>
+        <p>Votre paiement n'a pas été effectué.</p>
+      </div>
+    );
+  }
+  
---
+++ app/succes/page.tsx
@@ -0,0 +1,9 @@
+export default function SuccessPage() {
+    return (
+      <div>
+        <h1>Paiement réussi !</h1>
+        <p>Merci pour votre dépôt.</p>
+      </div>
+    );
+  }
+  
