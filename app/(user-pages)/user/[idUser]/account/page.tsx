6bdd64c5e31be3cfe0421c68469d168880acd500
LucaCode01 <jokyofficiel1@gmail.com>
2025-01-31 11:14:58 +0100

    Ajout du chart et de la liste des tokens du user

--- app/(user-pages)/user/[idUser]/account/page.tsx
+++ app/(user-pages)/user/[idUser]/account/page.tsx
@@ -1,7 +1,27 @@
-export default async function AccountPage() {
+import { TablesTokens } from "@/components/tables-tokens"
+import { getTokenOfUser } from "../action/userAccountAction"
+import { ChartUserAccount } from "@/components/chart-user-account"
+
+type Params = Promise<{ idUser: string }>
+
+export default async function AccountPage({
+    params,
+}: {
+    params: Params
+}) {
+    const { idUser } = await params
+    const tokens = await getTokenOfUser(idUser)
     return (
         <div className="w-full h-screen">
-            Hello
+            <div className="flex flex-col md:flex-row gap-4 px-10 py-4">
+                <div className="flex-1">
+                    <ChartUserAccount rows={tokens}/>
+                </div>
+                <div className="flex-1">
+                    <ChartUserAccount rows={tokens} />
+                </div>
+            </div>
+            <TablesTokens rows={tokens} />
         </div>
     )
 }
