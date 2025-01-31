8e1061b244d243bbb76c192b9bed5a39e3c09d57
LucaCode01 <jokyofficiel1@gmail.com>
2025-01-31 11:18:13 +0100

    Ajout du bouton vers le deposit

--- app/(user-pages)/user/[idUser]/account/page.tsx
+++ app/(user-pages)/user/[idUser]/account/page.tsx
@@ -1,6 +1,9 @@
 import { TablesTokens } from "@/components/tables-tokens"
 import { getTokenOfUser } from "../action/userAccountAction"
 import { ChartUserAccount } from "@/components/chart-user-account"
+import { Button } from "@heroui/react"
+import Link from "next/link"
+import { paths } from "@/paths"
 
 type Params = Promise<{ idUser: string }>
 
@@ -15,12 +18,19 @@ export default async function AccountPage({
         <div className="w-full h-screen">
             <div className="flex flex-col md:flex-row gap-4 px-10 py-4">
                 <div className="flex-1">
-                    <ChartUserAccount rows={tokens}/>
+                    <ChartUserAccount rows={tokens} />
                 </div>
                 <div className="flex-1">
                     <ChartUserAccount rows={tokens} />
                 </div>
             </div>
+            <Button
+                color="primary"
+                className="ml-10"
+                as={Link}
+                href={paths.userDeposit(idUser)}>
+                Déposer de l'argent
+            </Button>
             <TablesTokens rows={tokens} />
         </div>
     )
