e5fc408b5fd29ac788cc170f8f58cfb2ff72ce2c
Giovanni Salcuni <giovanni.salcuni12@gmail.com>
2025-02-25 11:58:52 +0100

    fix bug screen view

--- app/(user-pages)/user/[idUser]/billing/page.tsx
+++ app/(user-pages)/user/[idUser]/billing/page.tsx
@@ -10,7 +10,7 @@ export default async function Billing({ params }: { params: Promise<{ idUser: st
     const { idUser } = await params
     const invoices = await getUserBilling(idUser)
     return (
-        <div className="container mx-auto p-4 h-screen">
+        <div className="container mx-auto p-4 h-full">
           <Spacer y={12} />
             <h1 className="text-3xl font-extrabold text-center mb-8 bg-gradient-to-r from-gray-300 via-gray-100 to-gray-300 bg-clip-text text-transparent tracking-wide">
                 Historique des paiements
