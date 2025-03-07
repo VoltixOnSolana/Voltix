76e4609829b2e1362378c1ff27e1ed9a9dc58739
Enzo <enzo.brsc@eduge.ch>
2025-03-07 10:37:02 +0100

    Reverse invoice order in billing page for improved user experience

--- app/(user-pages)/user/[idUser]/billing/page.tsx
+++ app/(user-pages)/user/[idUser]/billing/page.tsx
@@ -5,16 +5,18 @@ import { getUserBilling } from "../action/userAccountAction"
 import React from 'react'
 import CardBilling from "@/components/card-billing"
 
+
 export default async function Billing({ params }: { params: Promise<{ idUser: string }> }) {
   const { idUser } = await params
   const invoices = await getUserBilling(idUser)
+  const invoicesReverse = invoices.reverse()
   return (
     <div className="container mx-auto p-4 h-full">
       <Spacer y={12} />
       <h1 className="text-3xl font-extrabold text-center mb-8 text-foreground">
         Historique des paiements
       </h1>
-      {invoices.map((invoice, index) => (
+      {invoicesReverse.map((invoice, index) => (
         <CardBilling 
           key={invoice.id} 
           invoice={invoice} 
